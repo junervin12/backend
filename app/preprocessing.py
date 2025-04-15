c7dbfaaf3ba627bc41200e6e2a2eead6159906b1
junervin12 <junervin12@gmail.com>
2025-04-15 22:58:41 +0700

    add new folder nltk_data_new append2

--- app/preprocessing.py
+++ app/preprocessing.py
@@ -3,7 +3,8 @@ import string
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 
-nltk.data.path.append('./nltk_data_new')
+nltk_data_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'nltk_data_new'))
+nltk.data.path.append(nltk_data_path)
 def preprocess_text(text):
     tokens = word_tokenize(text.lower())
     stop_words = set(stopwords.words('english'))
