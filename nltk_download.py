42b1be3e04c85cb1bca4c8b1172e4156f77a817f
junervin12 <junervin12@gmail.com>
2025-04-15 22:36:37 +0700

    add new folder nltk

--- nltk_download.py
+++ nltk_download.py
@@ -1,6 +1,6 @@
 import nltk
 
-nltk.download('punkt', download_dir='nltk_data')
-nltk.download('punkt_tab', download_dir='nltk_data')
-nltk.download('stopwords', download_dir='nltk_data')
-nltk.download('brown', download_dir='nltk_data')
+nltk.download('punkt', download_dir='nltk_data_new')
+nltk.download('punkt_tab', download_dir='nltk_data_new')
+nltk.download('stopwords', download_dir='nltk_data_new')
+nltk.download('brown', download_dir='nltk_data_new')
